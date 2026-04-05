ee223aad14d0b14ca6c9e59b043ca29a56bae9fa
Aldyth11 <aldyth.r.f@gmail.com>
2026-04-05 10:18:42 +0700

    Week 6 JS 6

--- PraktikumPWL/app/Filament/Resources/Posts/Schemas/PostForm.php
+++ PraktikumPWL/app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -22,56 +22,59 @@ class PostForm
     {
         return $schema
             ->components([
-                // Layout Horizontal: Left Section (Wider) & Right Section (Narrower)
+                Section::make('Post Details')
+                    ->description('Fill in the details of the post')
+                    ->icon('heroicon-o-document-text')
+                    ->schema([
+                        Group::make()
+                            ->schema([
+                                TextInput::make('title')
+                                ->required()
+                                ->rules('min:3|max:100'),
+                                TextInput::make('slug')
+                                ->required()
+                                ->unique()
+                                ->validationMessages([
+                                    'unique' => 'The slug must be unique.',
+                                ]),
+                                Select::make('category_id')
+                                    ->required()
+                                    ->relationship('category', 'name')
+                                    ->preload()
+                                    ->searchable(),
+                                ColorPicker::make('color')->required(),
+                            ])->columns(2),
+                        MarkdownEditor::make('content')->required(),
+                    ])
+                    ->columnSpan(2),
+
                 Group::make()
                     ->schema([
-                        // Left Column - Post Details (lebih lebar)
-                        Group::make()
+                        // Section 2 - Post Media
+                        Section::make('Post Media')
+                            ->description('Upload media related to the post')
+                            ->icon('heroicon-o-photo')
                             ->schema([
-                                Section::make('Post Details')
-                                    ->description('Fill in the details of the post')
-                                    ->icon('heroicon-o-document-text')
-                                    ->schema([
-                                        TextInput::make('title')->required(),
-                                        TextInput::make('slug')->required(),
-                                        Select::make('category_id')
-                                            ->relationship('category', 'name')
-                                            ->preload()
-                                            ->searchable(),
-                                        ColorPicker::make('color')->required(),
-                                    ])->columns(2),
-                                    MarkdownEditor::make('content')->required(),
-                            ])
-                            ->columnSpan(2), 
+                                FileUpload::make('image')
+                                    ->required()
+                                    ->disk('public')
+                                    ->directory('post'),
+                            ]),
 
-                        Group::make()
+                        // Section 3 - Post Metadata
+                        Section::make('Post Metadata')
+                            ->description('Additional information about the post')
+                            ->icon('heroicon-o-information-circle')
                             ->schema([
-                                // Section 2 - Post Media
-                                Section::make('Post Media')
-                                    ->description('Upload media related to the post')
-                                    ->icon('heroicon-o-photo')
-                                    ->schema([
-                                        FileUpload::make('image')
-                                            ->disk('public')
-                                            ->directory('post'),
-                                    ]),
-
-                                // Section 3 - Post Metadata
-                                Section::make('Post Metadata')
-                                    ->description('Additional information about the post')
-                                    ->icon('heroicon-o-information-circle')
-                                    ->schema([
-                                        TagsInput::make('tags'),
-                                        Checkbox::make('is_published')->label('Published'),
-                                        DateTimePicker::make('published_at')
-                                            ->format('Y-m-d H:i:s')
-                                            ->label('Publish Date'),
-                                    ]),
-
-                            ])
-                            ->columnSpan(1), // Kolom kanan lebih kecil (1/3)
+                                TagsInput::make('tags'),
+                                Checkbox::make('is_published')->label('Published'),
+                                DateTimePicker::make('published_at')
+                                    ->format('Y-m-d H:i:s')
+                                    ->label('Publish Date'),
+                            ]),
                     ])
-                    ->columns(3), 
-            ])->columns(1);
+                    ->columnSpan(1),
+            ])
+            ->columns(3);
     }
 }
