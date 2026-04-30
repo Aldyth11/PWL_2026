964ebf74f8ddcb65db859cf6dfe43bd63d1918ae
Aldyth11 <aldyth.r.f@gmail.com>
2026-04-30 18:06:24 +0700

    Week 11 JS 11

--- PraktikumPWL/app/Filament/Resources/Posts/Tables/PostsTable.php
+++ PraktikumPWL/app/Filament/Resources/Posts/Tables/PostsTable.php
@@ -9,6 +9,9 @@ use Filament\Tables\Table;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Columns\ColorColumn;
 use Filament\Tables\Columns\imageColumn;
+use Filament\Tables\Filters\Filter;
+use Filament\Forms\Components\DatePicker;
+use Filament\Tables\Filters\SelectFilter;
 
 class PostsTable
 {
@@ -18,13 +21,27 @@ class PostsTable
             ->columns([
                 //
                 TextColumn::make('title')->searchable()->sortable(),
+                TextColumn::make('slug')->searchable()->sortable(),
                 TextColumn::make('category.name')->label('Category')->searchable()->sortable(),
                 ColorColumn::make('color'),
                 TextColumn::make('published_at')->dateTime()->label('Published At')->sortable(),
                 ImageColumn::make('image')->disk('public')->label('Image'),
             ])->defaultSort('published_at', 'asc')
             ->filters([
-                //
+                Filter::make('published_at')
+                    ->form([
+                        DatePicker::make('published_at')->label('Published From'),
+                    ])
+                    ->query(function ($query, $data) {
+                    return $query->when(
+                        $data['published_at'],
+                        fn ($query, $date) => $query->whereDate('published_at', $date)
+                    );
+                }),
+                SelectFilter::make('category_id')
+                    ->label('Category')
+                    ->relationship('category', 'name')
+                    ->preload(),
             ])
             ->recordActions([
                 EditAction::make(),
